2ba0a1565dd2ed76df1e7c311dfb1bfa01bedd0c
Cameron More <123985185+cameronmore@users.noreply.github.com>
2024-08-24 15:04:44 -0400

    Added validator but needs fixing

--- src/scripts/Validator.py
+++ src/scripts/Validator.py
@@ -1,1 +1,15 @@
+import json
 import jsonschema
+
+MediaElementSchema = "src/schema/MediaElementSchema.json"
+
+def MediaElementSchemaValidator(jsonData):
+    try:
+        jsonschema.validate(json.loads(jsonData), MediaElementSchema)
+        return True
+    except jsonschema.ValidationError:
+        return False
+
+JSON_instance_sample = """{"title":"Love and Hate","author":{"firstName":"Robert","lastName":"Robertson"},"publisher":{"publisherName":"Penguin","location":"London"},"mediaType":"book","time":{"year":"2004"}}"""
+
+MediaElementSchemaValidator(JSON_instance_sample)
